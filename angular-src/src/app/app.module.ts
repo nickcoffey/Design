480a603c94bafeb577aa9110086573105abd0e9c
Coffey <P2742129@CORP.CHARTERCOM.COM>
2017-09-29 16:51:55 -0500

    Added navbar, routes, and components.

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,21 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdSidenavModule, MdToolbarModule } from '@angular/material';
+import { MdSidenavModule, MdToolbarModule, MdIconModule, MdButtonModule, MdMenuModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { CustomersComponent } from './components/customers/customers.component';
+
+const appRoutes:Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'customers', component: CustomersComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    CustomersComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MdSidenavModule,
-    MdToolbarModule
+    MdToolbarModule,
+    MdIconModule,
+    MdButtonModule,
+    MdMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
