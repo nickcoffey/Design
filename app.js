eb4da34dff7f7861bacf958023b0c6b944ff6d07
Coffey, Nick <nicholas.coffey@charter.com>
2017-11-29 10:17:03 -0600

    Moved deploy script out of project directory

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const port = process.env.PORT || 8080;
 // Git Hook Auto-Deploy
 app.post('/api/deploy', (req, res) => {
     var spawn = require('child_process').spawn,
-        deploy = spawn('sh', ['./deploy.sh']);
+        deploy = spawn('sh', ['../../Scripts/deploy.sh']);
     deploy.stdout.on('data', (data) => {
         console.log(''+data);
     });
--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Stopping app"
-forever stop app.js
-echo "Pulling in changes"
-git pull
-echo "Starting app"
-forever start app.js
