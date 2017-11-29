4a76da90037a0200d9debb6188ec42e495905efd
Coffey, Nick <nicholas.coffey@charter.com>
2017-11-29 11:11:45 -0600

    Removed 'sh' from exec

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const port = process.env.PORT || 8080;
 // Git Hook Auto-Deploy
 app.post('/api/deploy', (req, res) => {
     const exec = require('child_process').exec;
-    const deploy = exec('sh cd ../../Scripts/; ./deploy.sh;');
+    const deploy = exec('cd ../../Scripts/; ./deploy.sh;');
     deploy.stdout.on('data', (data) => {
         console.log(''+data);
     });
